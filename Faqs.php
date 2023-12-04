b0c78fff26dfe772e63619bcc0281214178f27a0
hrid00y <hridoyahmedddd@gmail.com>
2023-12-04 19:13:24 +0600

    add

--- Faqs.php
+++ Faqs.php
@@ -16,6 +16,31 @@
 
     <title>Faq page list - Bootdey.com</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  
+  <link href="img/favicon.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+
+  
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="assets/bootstrap-v3/css/bootstrap.css">
+  <link rel="stylesheet" href="assets/bootstrap-v3/css/bootstrap.css">
+  <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons" />
+
+  
+  <link href="css/style.css" rel="stylesheet">
     <link
       href="https://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
       rel="stylesheet"
